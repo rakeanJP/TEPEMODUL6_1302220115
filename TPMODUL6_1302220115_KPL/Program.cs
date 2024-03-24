8c3caf355521f54b44138ac8c0b69662c4aab182
rakean <rakean321@gmail.com>
2024-03-25 04:59:40 +0700

    commit pertama

---
+++ TPMODUL6_1302220115_KPL/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPMODUL6_1302220115_KPL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
